161f2c61d6139abd09ad309a856d7ceaac8d87e7
tanerijun <tanerivince@gmail.com>
2023-01-06 23:03:10 +0800

    Test: Todos should render a todo item when the user add it

--- src/components/__tests__/Todo.test.ts
+++ src/components/__tests__/Todo.test.ts
@@ -1,5 +1,21 @@
-import Todo from '../Todo.svelte'
+import { fireEvent, render, screen } from '@testing-library/svelte'
+import Todos from '../Todos.svelte'
 
-describe('Test', () => {
-	expect(1 + 1).toBeInTheDocument()
+afterEach(() => {
+	localStorage.clear()
+})
+
+describe('Testing Todos', () => {
+	it('should render a todo item when the user add it', async () => {
+		render(Todos)
+		let value = 'Todo item'
+		let todoInputElement = screen.getByPlaceholderText(
+			/what needs to be done?/i
+		)
+
+		await fireEvent.input(todoInputElement, { target: { value } })
+		await fireEvent.submit(todoInputElement)
+
+		expect(screen.getByText(value)).toBeInTheDocument()
+	})
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path'
 
 export default defineConfig({
 	plugins: [svelte({ hot: !process.env.VITEST })],
+	resolve: {
+		alias: {
+			$root: path.resolve('./src'),
+		},
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 		globals: true,
