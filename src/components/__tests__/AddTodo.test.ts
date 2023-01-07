0cd1b998eaa6f507c95e0d3865fdfee3180d8439
tanerijun <tanerivince@gmail.com>
2023-01-07 22:26:07 +0800

    Reformat test

--- src/components/__tests__/AddTodo.test.ts
+++ src/components/__tests__/AddTodo.test.ts
@@ -12,30 +12,32 @@ function renderAddTodo() {
 	return props
 }
 
-it('should focus input on page load', () => {
-	renderAddTodo()
-	let todoInputElement = screen.getByPlaceholderText(/what needs to be done/i)
-	expect(todoInputElement).toHaveFocus()
-})
+describe('Testing AddTodo', () => {
+	it('should focus input on page load', () => {
+		renderAddTodo()
+		let todoInputElement = screen.getByPlaceholderText(/what needs to be done/i)
+		expect(todoInputElement).toHaveFocus()
+	})
 
-it('should allow user to type into the input', async () => {
-	renderAddTodo()
+	it('should allow user to type into the input', async () => {
+		renderAddTodo()
 
-	let inputValue = 'Todo Item'
+		let inputValue = 'Todo Item'
 
-	let todoInputElement = screen.getByPlaceholderText(/what needs to be done/i)
-	await fireEvent.change(todoInputElement, { target: { value: inputValue } })
-	expect(todoInputElement).toHaveValue(inputValue)
-})
+		let todoInputElement = screen.getByPlaceholderText(/what needs to be done/i)
+		await fireEvent.change(todoInputElement, { target: { value: inputValue } })
+		expect(todoInputElement).toHaveValue(inputValue)
+	})
 
-test('should allow user to submit todo', async () => {
-	let { addTodo } = renderAddTodo()
+	test('should allow user to submit todo', async () => {
+		let { addTodo } = renderAddTodo()
 
-	let inputValue = 'Todo Item'
+		let inputValue = 'Todo Item'
 
-	let todoInputElement = screen.getByPlaceholderText(/what needs to be done/i)
-	await fireEvent.change(todoInputElement, { target: { value: inputValue } })
-	await fireEvent.submit(todoInputElement)
+		let todoInputElement = screen.getByPlaceholderText(/what needs to be done/i)
+		await fireEvent.change(todoInputElement, { target: { value: inputValue } })
+		await fireEvent.submit(todoInputElement)
 
-	expect(addTodo).toHaveBeenCalledTimes(1)
+		expect(addTodo).toHaveBeenCalledTimes(1)
+	})
 })
